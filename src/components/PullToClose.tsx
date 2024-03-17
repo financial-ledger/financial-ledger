8237837b6271f037eb4b0ed481bf124b0eacf4f1
lifeisegg123 <leejj2002@naver.com>
2024-03-17 13:33:08 +0900

    feat: PullToClose

---
+++ src/components/PullToClose.tsx
@@ -0,0 +1,74 @@
+import { ReactNode, useRef, useState } from 'react';
+
+import { m, PanInfo } from 'framer-motion';
+import { LazyDomMaxMotion } from 'src/ui/framer/LazyDomMaxMotion';
+
+interface PullToCloseProps {
+  children: ReactNode;
+  className?: string;
+  onClose: VoidFunction;
+  threshold?: number;
+}
+
+const THRESHOLD = 0.3;
+
+/**
+ * AnimatePresence와 함께 사용 권장
+ */
+export function PullToClose({
+  onClose,
+  className,
+  children,
+  threshold = THRESHOLD,
+}: PullToCloseProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [snapToOrigin, setSnapToOrigin] = useState(false);
+  const handleDragEnd = (_: any, info: PanInfo) => {
+    if (!ref.current) return;
+
+    if (
+      ref.current.getBoundingClientRect().height * threshold <
+      info.offset.y
+    ) {
+      onClose();
+    }
+  };
+
+  const handleDrag = (_: any, info: PanInfo) => {
+    requestAnimationFrame(() => {
+      if (!ref.current) return;
+      if (
+        ref.current.getBoundingClientRect().height * threshold <
+        info.offset.y
+      ) {
+        setSnapToOrigin(false);
+      } else {
+        setSnapToOrigin(true);
+      }
+    });
+  };
+
+  return (
+    <LazyDomMaxMotion>
+      <m.div
+        animate={{ y: '0%' }}
+        className={className}
+        drag="y"
+        dragConstraints={{ top: 0 }}
+        dragElastic={0}
+        dragSnapToOrigin={snapToOrigin}
+        exit={{ y: '100%' }}
+        initial={{ y: '100%' }}
+        ref={ref}
+        transition={{
+          duration: !snapToOrigin ? 0.1 : 0.2,
+          type: 'tween',
+        }}
+        onDrag={handleDrag}
+        onDragEnd={handleDragEnd}
+      >
+        {children}
+      </m.div>
+    </LazyDomMaxMotion>
+  );
+}
